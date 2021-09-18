4d71d434b7fb25d7d149dede01fab9c22857be25
ViniRLima <54587708+ViniRLima@users.noreply.github.com>
2021-09-17 21:57:02 -0300

    Add files via upload

---
+++ Projeto Rental/Senai.Rental.WebApi/Senai.Rental.WebApi/Interfaces/IClienteRepository.cs
@@ -0,0 +1,53 @@
+﻿using Senai.Rental.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Rental.WebApi.Interfaces
+{
+    /// <summary>
+    /// Interface responsável pelo repositório ClienteRepository
+    /// </summary>
+    interface IClienteRepository
+    {
+        /// <summary>
+        /// Listar todos os Clientes
+        /// </summary>
+        /// <returns> Lista de Clientes </returns>
+        List<ClienteDomain> ListarTodos();
+
+        /// <summary>
+        /// Busca um cliente através do seu id
+        /// </summary>
+        /// <param name="idCliente">id do cliente que será buscado</param>
+        /// <returns>Um cliente buscado</returns>
+        ClienteDomain BuscarPorId(int idCliente);
+
+        /// <summary>
+        /// Cadastra um novo cliente
+        /// </summary>
+        /// <param name="novoCliente">Objeto novoCliente com os novos dados</param>
+        void Cadastrar(ClienteDomain novoCliente);
+
+        /// <summary>
+        /// Atualiza um cliente existente
+        /// </summary>
+        /// <param name="clienteAtualizado">Objeto com as novos atualizados</param>
+        void AtualizarIdCorpo(ClienteDomain clienteAtualizado);
+
+        /// <summary>
+        /// Atualiza um cliente existente
+        /// </summary>
+        /// <param name="idCliente">id do cliente que será atualizado</param>
+        /// <param name="clienteAtualizado">Objeto clienteAtulizado com os novos dados atualizados</param>
+        void AtualizarIdUrl(int idCliente, ClienteDomain clienteAtualizado);
+
+        /// <summary>
+        /// Deleta um cliente existente
+        /// </summary>
+        /// <param name="idCLiente">id do cliente que será deletado</param>
+        void Deletar(int idCLiente);
+    }
+
+}
---
+++ Projeto Rental/Senai.Rental.WebApi/Senai.Rental.WebApi/Interfaces/IMarcaRepository.cs
@@ -0,0 +1,52 @@
+﻿using Senai.Rental.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Rental.WebApi.Interfaces
+{
+    /// <summary>
+    /// Interface responsável pelo repositório MarcaRepository
+    /// </summary>
+    interface IMarcaRepository
+    {
+        /// <summary>
+        /// Listar todas as Marcas
+        /// </summary>
+        /// <returns>Lista de Marcas</returns>
+        List<MarcaDomain> ListarTodos();
+
+        /// <summary>
+        /// Busca uma marca através do seu id
+        /// </summary>
+        /// <param name="idMarca">id da marca que será buscada</param>
+        /// <returns>Uma marca buscada</returns>
+        MarcaDomain BuscarPorId(int idMarca);
+
+        /// <summary>
+        /// Cadastra uma nova Marca
+        /// </summary>
+        /// <param name="novaMarca">Objeto novaMarca com os novos dados</param>
+        void Cadastrar(MarcaDomain novaMarca);
+
+        /// <summary>
+        /// Atualiza uma marca existente
+        /// </summary>
+        /// <param name="marcaAtualizada">Objeto com as novos dados atualizados</param>
+        void AtualizarIdCorpo(MarcaDomain marcaAtualizada);
+
+        /// <summary>
+        /// Atualiza uma marca existente
+        /// </summary>
+        /// <param name="idMarca">id da marca que será atualizada</param>
+        /// <param name="marcaAtualizada">Objeto marcaAtualizada com os novos dados atualizados</param>
+        void AtualizarIdUrl(int idMarca, MarcaDomain marcaAtualizada);
+
+        /// <summary>
+        /// Deleta uma marca existente
+        /// </summary>
+        /// <param name="idMarca">id da marca que será deletada</param>
+        void Deletar(int idMarca);
+    }
+}
